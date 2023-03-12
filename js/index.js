d7b5664088349834652a798d0a32b859b7a4a9e1
GalSanyi <86233181+GalSanyi@users.noreply.github.com>
2023-03-12 21:44:23 +0100

    card

--- js/index.js
+++ js/index.js
@@ -1,1 +1,18 @@
-console.log("hello!");
+const square = document.querySelectorAll(".square");
+square.forEach((item) => {
+  item.addEventListener("click", () => {
+    document.querySelectorAll(".pagination__card li").forEach((item) => {
+      item.classList.remove("active");
+      item.classList.add("inactive");
+    });
+    item.classList.add("active");
+    item.classList.remove("inactive");
+  });
+});
+
+const heart = document.querySelectorAll("[data-menu-button]");
+heart.forEach((item) => {
+  item.addEventListener("click", () => {
+    item.classList.toggle("is-open");
+  });
+});
